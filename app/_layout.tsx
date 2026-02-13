7b426123de6b98a69e38900ae03ad87bfd882222
Saubhagya-2004 <saubhagyabaliarsingh2@gmail.com>
2026-02-13 23:34:04 +0530

    intial commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,16 +2,25 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-
+import '../global.css'
 import { useColorScheme } from '@/hooks/use-color-scheme';
-
+import {useFonts} from 'expo-font'
+import { ActivityIndicator } from 'react-native';
 export const unstable_settings = {
   anchor: '(tabs)',
 };
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-
+  const [fontloaded] = useFonts({
+'appFont':require('./../assets/fonts/PlaywriteNZBasic-Regular.ttf'),
+'appBold': require('./../assets/fonts/PlaywriteNZBasic-Thin.ttf') 
+  })
+  if(!fontloaded){
+    {
+      return <ActivityIndicator/>
+    }
+  }
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
