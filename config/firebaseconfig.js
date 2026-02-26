d083dc1017ae101513de5a12b6fa6bf11a29e288
Saubhagya-2004 <saubhagyabaliarsingh2@gmail.com>
2026-02-26 17:00:32 +0530

    five 5.5

--- config/firebaseconfig.js
+++ config/firebaseconfig.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCU0Y5y9632Gfo-3JBVFuFt_qy5_ojx6l0",
-  authDomain: "dinner-time-551f3.firebaseapp.com",
-  projectId: "dinner-time-551f3",
-  storageBucket: "dinner-time-551f3.firebasestorage.app",
-  messagingSenderId: "436755467457",
-  appId: "1:436755467457:web:4026e3a791ee3b28fd1840",
-  measurementId: "G-6QVCLRP92B"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
